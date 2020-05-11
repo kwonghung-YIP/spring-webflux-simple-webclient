c4c27b246158b1687fcdc4ab1aefe858d854dc9a
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2020-05-12 03:24:24 +0800

    test the client

--- src/main/java/org/hung/CmdRunner2.java
+++ src/main/java/org/hung/CmdRunner2.java
@@ -1,45 +1,51 @@
 package org.hung;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorResourceFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import io.netty.handler.ssl.SslContextBuilder;
 import lombok.extern.slf4j.Slf4j;
-import reactor.netty.http.client.HttpClient;
 
 @Slf4j
-//@Component
-public class CmdRunner implements CommandLineRunner{
-
+@Component
+public class CmdRunner2 implements CommandLineRunner{
+	
 	@Value("file:C:\\Users\\kwong\\Documents\\certs\\selfsigned\\ca\\my-rootCA.crt")
 	private Resource trustCertFile;
 	
+	@Autowired
+	private ReactorResourceFactory factory;
+	
 	@Override
 	public void run(String... args) throws Exception {
+	
 		SslContextBuilder nettySslContextBuilder = 
 		  SslContextBuilder
 	        .forClient()
               .trustManager(trustCertFile.getInputStream());
-        
-		HttpClient reactorHttpClient = 
-		  HttpClient
-		    .create()
-		      .secure(spec -> spec.sslContext(nettySslContextBuilder));
-		    
+				
+		ReactorClientHttpConnector connector = new ReactorClientHttpConnector(factory,httpClient -> {
+			httpClient.secure(spec -> spec.sslContext(nettySslContextBuilder));
+			return httpClient;
+		});
+		
 		WebClient client = 
 		  WebClient
 		    .builder()
-		      .clientConnector(new ReactorClientHttpConnector(reactorHttpClient))
+		      .clientConnector(connector)
 		        .baseUrl("https://localhost:8443")
 		          .filter(ExchangeFilterFunctions.basicAuthentication("admin", "password"))
 		            .build();
-		/*
+
 		client
 		  .get()
 		    .uri("/interval")
@@ -49,16 +55,14 @@ public class CmdRunner implements CommandLineRunner{
 		    .subscribe(l -> {
 		    	log.info("I got a {}",l);
 		    });
-		*/
-		
-		client
-		  .get()
-		    .uri("/echo")
-		      .retrieve()
-		        .bodyToMono(String.class)
-		          .subscribe(string -> {
-		        	 log.info(string); 
-		          });
+
+	}
+
+	@Bean
+	ReactorResourceFactory reactorResourceFactory() {
+		ReactorResourceFactory factory = new ReactorResourceFactory();
+		factory.setUseGlobalResources(false);
+		return factory;
 	}
 
 }
